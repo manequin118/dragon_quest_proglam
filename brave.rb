7cccfc9411b69c083cd64be17b6704a01eac6d64
chu <chuusatetsuya@chuusatetsuyanoMacBook-Pro.local>
2021-02-12 21:56:40 +0900

    if文再度修正

--- brave.rb
+++ brave.rb
@@ -1,12 +1,14 @@
 #encoding: utf-8
 class Brave < Character
   def attack(monster)
-    damage1 = (offense - monster.defense) / 2
-    monster.hp -= damage1
-    monster.hp = 0 if monster.hp < 0
-    puts <<~EOS if hp > 0
-      "#{name}の攻撃！"
-      "#{monster.name}に#{damage1}のダメージを与えた!"
-    EOS
+    if hp > 0
+      damage1 = (offense - monster.defense) / 2
+      monster.hp -= damage1
+      monster.hp = 0 if monster.hp < 0
+      puts <<~EOS
+             "#{name}の攻撃！"
+             "#{monster.name}に#{damage1}のダメージを与えた!"
+           EOS
+    end
   end
 end
--- monster.rb
+++ monster.rb
@@ -1,12 +1,14 @@
 #encoding: utf-8
 class Monster < Character
   def attack(brave)
-    damage2 = (offense - brave.defense) / 2
-    brave.hp -= damage2
-    brave.hp = 0 if brave.hp < 0
-    puts <<~EOS if hp > 0
-      "#{name}の攻撃！"
-      "#{brave.name}は#{damage2}のダメージを受けた!"
-    EOS
+    if hp > 0
+      damage2 = (offense - brave.defense) / 2
+      brave.hp -= damage2
+      brave.hp = 0 if brave.hp < 0
+      puts <<~EOS
+             "#{name}の攻撃！"
+             "#{brave.name}は#{damage2}のダメージを受けた!"
+           EOS
+    end
   end
 end
